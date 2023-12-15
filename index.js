d16156562c960a3c715db5c966475acbfdb3ead7
Pontus <minecraftpontus@gmail.com>
2023-12-15 12:17:00 +0100

    added a PORT and tested some get requests

--- index.js
+++ index.js
@@ -3,8 +3,39 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 const Joi = require('joi');
-const axios = require('axios');
+const { env } = require('process');
+const axios = require('axios').default;
+axios.defaults.baseURL = 'https://swapi.dev/api';
+
+// ### 1. **Add Star Wars Characters:**
+
+// - Implement an endpoint to add Star Wars characters to your API.
+// - Validate the character name against the Swapi database.
+// - If the character exists, add it to your API's data storage.
 
 
 
 
+
+
+//GET requests
+axios.get('/people/3')
+    .then((response) => console.log((response.data.name)))
+    .catch((error) => console.log((error.message)));
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+
+//POST requests
+
+app.post('/add-charater', (req, res) => {
+
+})
+
+
+//PUT requests
+//DELETE requests
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on Port: ${port}...`));
