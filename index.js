a6ed350d972456e17140b81c3ee71b344c52fa98
Pontus <minecraftpontus@gmail.com>
2023-12-15 10:17:01 +0100

    added npm packages and .gitignore

---
+++ index.js
@@ -0,0 +1,10 @@
+// npm that I need to require: express, Joi, axios
+const express = require('express');
+const app = express();
+app.use(express.json());
+const Joi = require('joi');
+const axios = require('axios');
+
+
+
+
