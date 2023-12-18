d04322b529595420332c00e59c23566d42b95c25
Pontus <minecraftpontus@gmail.com>
2023-12-18 15:32:48 +0100

    Finished the 3rd DoD (PUT request).

--- index.js
+++ index.js
@@ -3,23 +3,15 @@ const { log } = require('console');
 const express = require('express');
 const app = express();
 app.use(express.json());
-const Joi = require('joi');
 const { env } = require('process');
 const axios = require('axios').default;
 axios.defaults.baseURL = 'https://swapi.dev/api';
 
-// ### 1. **Add Star Wars Characters:**
-
-// - Implement an endpoint to add Star Wars characters to your API.
-// - Validate the character name against the Swapi database.
-// - If the character exists, add it to your API's data storage.
-
-
-
 const characters = [
-    { id: 1, name: 'Pontus Norup' },
-    { id: 2, name: 'Marvin Benno' },
-    { id: 3, name: 'Noel Lundvist' },
+    { id: 1, name: 'Pontus Skywalker' },
+    { id: 2, name: 'Emperor Benno' },
+    { id: 3, name: 'Darth Skontan' },
+    { id: 4, name: 'Stromtrooper Noel' },
 ];
 
 
@@ -32,14 +24,23 @@ app.get('/collection', (req, res) => {
     res.send(characters);
 });
 
-//POST requests
+app.get('/collection/:id', (req, res) => {
+    const character = characters.find(c => c.id === parseInt(req.params.id));
+    if (!character) res.status(404).send('Given character was not found');
 
+    res.send(character);
+});
+
+//POST requests
 app.post('/add-character', async (req, res) => {
     try {
         const characterName = req.body.name;
 
-        const swapiResponse = await axios.get(`https://swapi.dev/api/people/?search=${characterName}`);
+        //Searches the swapi database from the given characterName.
+        const swapiResponse = await axios.get(`/people/?search=${characterName}`);
+        //Gets the data.name from the response and store it inside swapiName.
         const swapiName = swapiResponse.data.results[0].name;
+
 
         if (characterName === swapiName) {
             const newCharacter = {
@@ -51,20 +52,62 @@ app.post('/add-character', async (req, res) => {
             res.send(`${newCharacter.name} has been added to the collection.`);
         }
 
+        //The req.body.name can be 'Obi' and the swapi database returs the fullname. There for this else notice.
         else res.send(`Name input is wrong, maybe you meant ${swapiName}?`);
     }
 
     catch (error) {
-        console.error('The character does not exist in the Swapi database.', error.message);
+        //Diplays error if the req.body.name is not found in swapi database.
+        res.send('The character does not exist in the Swapi database.', error.message);
     }
 });
 
 
 //PUT requests
+
+app.put('/swap-characters', (req, res) => {
+    try {
+        //Validate if the req structure is correctly formatted
+        if (!req.body.characters || !Array.isArray(req.body.characters) || req.body.characters.length !== 2) return res.status(400).send('Invalid Request Structure');
+
+        //Validate if the 2 names is found in the "characters array" otherwise error
+        const invalidCharacters = req.body.characters.filter((char) => {
+            return !characters.some((c) => c.name === char.name);
+        });
+        if (invalidCharacters.length === 1) return res.status(404).send('One character is invalid');
+        if (invalidCharacters.length === 2) return res.status(404).send('Both characters is invalid');
+
+        // Extract character names from the request body
+        const [characterName1, characterName2] = req.body.characters.map((c) => c.name);
+
+        // Find the characters in the "characters" array
+        const character1 = characters.find((char) => char.name === characterName1);
+        const character2 = characters.find((char) => char.name === characterName2);
+
+        // Swap the characters in the array
+        const index1 = characters.indexOf(character1);
+        const index2 = characters.indexOf(character2);
+
+        characters[index1] = character2;
+        characters[index2] = character1;
+
+        res.send(`You have swapped places with ${character1.name} and ${character2.name} succesfully!`);
+    }
+    catch (error) {
+        console.error(error.message);
+    }
+});
+
 //DELETE requests
+app.delete('/collection/:id', (req, res) => {
+    const character = characters.find(c => c.id === parseInt(req.params.id));
+    if (!character) res.status(404).send('Given character was not found');
 
-//validation functions
+    const index = characters.indexOf(character);
 
+    if (character) characters.splice(index, 1);
+    res.send(`The Character ${character.name} was removed.`);
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on Port: ${port}...`));
