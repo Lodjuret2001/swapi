4ebff8f6847ba744efed574f03dc6210faa7572a
Pontus <minecraftpontus@gmail.com>
2023-12-18 16:43:22 +0100

    Clean up the code

--- index.js
+++ index.js
@@ -122,15 +122,3 @@ app.delete('/collection/:id', (req, res) => {
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on Port: ${port}...`));
-
-
-// {
-//     "characters": [
-//         {
-//             "name": "Emperor Marvin"
-//         },
-//         {
-//             "name": "Darth Skontan"
-//         }
-//     ]
-// }
